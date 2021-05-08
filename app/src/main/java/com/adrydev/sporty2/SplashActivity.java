726a09993c263662e19f9121b97503ce91e30a26
Adrian Gonzalez <agpersonalcoach@gmail.com>
2021-05-08 22:30:33 +0200

    Initial commit

---
+++ app/src/main/java/com/adrydev/sporty2/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.adrydev.sporty2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+    // iniciamos la variable en 3 segundos de duracion
+    private final int duracionSplash = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },duracionSplash);
+
+
+    }
+}
