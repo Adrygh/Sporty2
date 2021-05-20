c006e785ee312c9ac83b37ef01c25bde1ec49fe2
Adrian Gonzalez <agpersonalcoach@gmail.com>
2021-05-20 14:26:43 +0200

    nuevo

--- app/src/main/java/com/adrydev/sporty2/FutbolMap.java
+++ app/src/main/java/com/adrydev/sporty2/FutbolMap.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -38,9 +39,9 @@ public class FutbolMap extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng imf = new LatLng(40.40236439713283,-3.6986715139833564);
+        mMap.addMarker(new MarkerOptions().position(imf).title("Centro de estudios").snippet("Centro de estudios brutal").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+
+
     }
 }
