5c74fda2a2910d3b1f8c945f21474f06b1f8ec26
Adrian Gonzalez <agpersonalcoach@gmail.com>
2021-05-20 13:37:32 +0200

    generacion de intentens y logica

--- app/src/main/java/com/adrydev/sporty2/MainActivity.java
+++ app/src/main/java/com/adrydev/sporty2/MainActivity.java
@@ -1,14 +1,76 @@
 package com.adrydev.sporty2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnFut, btnBal, btnSka, btnPad, btnMisReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFut = (Button) findViewById(R.id.btnFutbol);
+        btnBal = (Button) findViewById(R.id.btnBaloncesto);
+        btnSka = (Button) findViewById(R.id.btnSkate);
+        btnPad = (Button) findViewById(R.id.btnPadel);
+        btnMisReg = (Button) findViewById(R.id.btnRegistros);
+
+        btnFut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),FutbolMap.class);
+                startActivity(intent);
+            }
+        });
+        btnBal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),BaloncestoMap.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SkateMap.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),PadelMap.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMisReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Registros.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/adrydev/sporty2/Registros.java
@@ -0,0 +1,14 @@
+package com.adrydev.sporty2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registros);
+    }
+}
