0c44bae885c8d3adc459421857852b7c7f68f80e
Adrian Gonzalez <agpersonalcoach@gmail.com>
2021-05-22 03:10:33 +0200

    metodo update raro mirailo

--- app/src/main/java/com/adrydev/sporty2/Registros.java
+++ app/src/main/java/com/adrydev/sporty2/Registros.java
@@ -21,6 +21,7 @@ public class Registros extends AppCompatActivity {
         final EditText edPes = findViewById(R.id.etPesoReg);
         final EditText edEdad = findViewById(R.id.etEdadReg);
         Button btnEnviar = findViewById(R.id.btnEnviar);
+        Button btnActualizar = findViewById(R.id.btnActualiza);
         DAOUsuario dao = new DAOUsuario();
 
         btnEnviar.setOnClickListener(v -> {
@@ -32,7 +33,21 @@ public class Registros extends AppCompatActivity {
       {
            Toast.makeText(this, ""+er.getMessage(), Toast.LENGTH_SHORT).show();
       }); });
+        btnActualizar.setOnClickListener(v -> {
+            HashMap<String,Object> hashMap = new HashMap<>();
+            hashMap.put("nombre",edName.getText().toString());
+            hashMap.put("deporte",edDepor.getText().toString());
+            hashMap.put("peso",edPes.getText().toString());
+            hashMap.put("edad",edEdad.getText().toString());
 
+            dao.update("usuario",hashMap).addOnSuccessListener(suc->
+           {
+                Toast.makeText(this, "Se inserto con exito", Toast.LENGTH_SHORT).show();
+           }).addOnFailureListener(er->
+            {
+                Toast.makeText(this, ""+er.getMessage(), Toast.LENGTH_SHORT).show();
+            });
+        });
 
     }
 }
