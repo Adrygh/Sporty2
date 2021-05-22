fb2c1cd2bb23cc86f9919cff27881633dab3f3c7
Adrian Gonzalez <agpersonalcoach@gmail.com>
2021-05-22 13:23:28 +0200

    creo que terminada

--- app/src/main/java/com/adrydev/sporty2/MainActivity.java
+++ app/src/main/java/com/adrydev/sporty2/MainActivity.java
@@ -57,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),Datos.class);
                 startActivity(intent);
+
+
             }
         });
 
